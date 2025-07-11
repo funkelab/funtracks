cea64b2a6b518874c5f1aa266aa9f57999c59bd4
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-07-11 19:31:15 -0400

    Add docstrings for time and position features

--- src/funtracks/features/node_features.py
+++ src/funtracks/features/node_features.py
@@ -7,7 +7,14 @@ from .feature import (
 
 
 class Time(Feature):
+    """A feature to hold the integer time frame a node is in"""
+
     def __init__(self, key: str | None = None):
+        """
+        Args:
+            key (str | None, optional): The key used to access the time feature on the
+                graph. Defaults to None, which sets it to "time".
+        """
         super().__init__(
             key=key if key is not None else "time",
             feature_type=FeatureType.NODE,
@@ -18,16 +25,19 @@ class Time(Feature):
 
 
 class Position(Feature):
-    """_summary_
-
-    Set recompute to True if there is a segmentation.
-    Must provide axes to get number of values.
-
-    Args:
-        Feature (_type_): _description_
-    """
+    """A feature to hold the position of a node (time not included)"""
 
     def __init__(self, axes: list[str], key: str | None = None, recompute: bool = False):
+        """
+        Args:
+            axes (list[str]): A list of the axis names. Used to infer the number
+                of dimensions of the position values.
+            key (str | None, optional): The key used to access the position on the graph.
+                Defaults to None, which sets it to "pos". The position must be stored
+                in a single key, not each dimension in a separate key.
+            recompute (bool, optional): Whether or not to recompute the position when
+                node (segmentations) are updated. Defaults to False.
+        """
         super().__init__(
             key=key if key is not None else "pos",
             display_name=axes,
