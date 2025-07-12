75ad7f3f344fd7db47e89efc77bbf31569576e2c
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-07-11 21:32:53 -0400

    Make feature class json serializable

--- src/funtracks/features/feature.py
+++ src/funtracks/features/feature.py
@@ -3,12 +3,18 @@ from __future__ import annotations
 from enum import Enum
 from typing import Any, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer, field_validator
 
 
 class FeatureType(Enum):
     NODE = "node"
     EDGE = "edge"
+
+
+class ValueType(Enum):
+    int = int
+    float = float
+    str = str
 
 
 class Feature(BaseModel):
@@ -18,8 +24,10 @@ class Feature(BaseModel):
         key (str): The string used to store the feature on the graph.
         feature_type (FeatureType): Specifies which graph elements the feature applies
             to, nodes or edges.
-        value_type (type): The data type of the feature values. Expected to be python
-            or numpy native types, not classes. Can be strings, but not recommended.
+        value_type (ValueType | type | str): The data type of the feature values.
+            Values are expected to be python or numpy native types, not classes.
+            Can pass in: the enum feilds, a string literal ("int", "float", or "str"),
+            or the enum values (python builtins `int`, `float`, or `str`)
         num_values (int, optional): The number of values expected for this feature.
             Defaults to 1.
         valid_ndim (tuple[Literal[3, 4], ...], optional): Is the feature valid in 3D
@@ -37,7 +45,7 @@ class Feature(BaseModel):
 
     key: str
     feature_type: FeatureType
-    value_type: type
+    value_type: ValueType
     num_values: int = 1
     valid_ndim: tuple[Literal[3, 4], ...] = (3, 4)
     display_name: str | list[str] | None = None
@@ -50,3 +58,17 @@ class Feature(BaseModel):
 
     def __str__(self) -> str:
         return self.feature_type.value + "_" + self.key
+
+    @field_serializer("value_type", when_used="json")
+    def _serialize_value_type(self, value_type: ValueType):
+        return value_type.name
+
+    @field_validator("value_type", mode="before")
+    @classmethod
+    def _validate_value_type(cls, value):
+        if isinstance(value, str):
+            try:
+                return ValueType[value]
+            except KeyError:
+                return value
+        return value
--- tests/features/test_feature.py
+++ tests/features/test_feature.py
@@ -1,8 +1,8 @@
-import numpy as np
 import pytest
 from pydantic import ValidationError
 
 from funtracks.features import Feature, FeatureType
+from funtracks.features.feature import ValueType
 
 
 def test_base_feature():
@@ -12,14 +12,16 @@ def test_base_feature():
         feat = Feature(key="test")
     with pytest.raises(ValidationError):
         feat = Feature(key="test", feature_type=FeatureType.NODE)
-    feat = Feature(key="test", feature_type=FeatureType.NODE, value_type=int)
+    with pytest.raises(ValidationError):
+        feat = Feature(key="test", feature_type=FeatureType.NODE, value_type="asdfj")
+    feat = Feature(key="test", feature_type=FeatureType.NODE, value_type="int")
     assert hash(feat) == hash(("node", "test"))
     assert str(feat) == "node_test"
 
     feat = Feature(
         key="computed",
         feature_type=FeatureType.EDGE,
-        value_type=np.float32,
+        value_type=float,
         num_values=3,
         display_name=["1", "2", "3"],
         recompute=True,
@@ -30,8 +32,25 @@ def test_base_feature():
     assert hash(feat) == hash(("edge", "computed"))
     assert feat.key == "computed"
     assert feat.feature_type == FeatureType.EDGE
-    assert feat.value_type == np.float32
+    assert feat.value_type == ValueType.float
     assert feat.display_name == ["1", "2", "3"]
     assert feat.recompute
     assert not feat.required
     assert feat.default_value == 0.0
+
+
+def test_json_dump():
+    feat = Feature(
+        key="computed",
+        feature_type=FeatureType.EDGE,
+        value_type=ValueType.float,
+        num_values=3,
+        display_name=["1", "2", "3"],
+        recompute=True,
+        required=False,
+        default_value=0.0,
+    )
+    json_dict = feat.model_dump(mode="json")
+
+    imported_feat = Feature(**json_dict)
+    assert imported_feat == feat
