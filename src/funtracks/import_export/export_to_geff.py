b18deb7a52399eac68afd1aca1ba0728716bf293
AnniekStok <anniek.stokkermans@gmail.com>
2025-07-18 11:22:23 -0400

    check if the directory exists and is empty

--- src/funtracks/import_export/export_to_geff.py
+++ src/funtracks/import_export/export_to_geff.py
@@ -9,5 +9,19 @@ if TYPE_CHECKING:
     from funtracks.data_model.tracks import Tracks
 
 def export_to_geff(tracks: Tracks, directory: Path):
-    axis_names = ["y", "x"] if len(tracks.scale) == 3 else ["z", "y", "x"]
+    """Export the Tracks nxgraph to geff.
+    Args: 
+        tracks (Tracks): tracks object containing a graph to save.
+        directory (Path): destination for saving the zarr.
+    """
+    
+    if directory.exists():
+        if not directory.is_dir():
+            raise ValueError(f"Provided path {directory} is not a directory.")
+        if any(directory.iterdir()):
+            raise ValueError(f"Directory {directory} is not empty.")
+    else:
+        raise ValueError(f"Directory {directory} does not exist.")
+    
+    axis_names = ["y", "x"] if tracks.ndim == 3 else ["z", "y", "x"]
     geff.write_nx(tracks.graph, directory, tracks.pos_attr, axis_names)
