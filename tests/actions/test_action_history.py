7ec936fe865d23e902a969b5498025a1a7ac5dfc
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-07-29 16:11:49 -0400

    Update tests to use singleton AddNode action

--- tests/actions/test_action_history.py
+++ tests/actions/test_action_history.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
-from funtracks.data_model.action_history import ActionHistory
-from funtracks.data_model.actions import AddNodes
+from funtracks.actions.action_history import ActionHistory
+from funtracks.actions import AddNode
 from funtracks.data_model.tracks import Tracks
 
 # https://github.com/zaboople/klonk/blob/master/TheGURQ.md
@@ -10,8 +10,8 @@ from funtracks.data_model.tracks import Tracks
 def test_action_history():
     history = ActionHistory()
     tracks = Tracks(nx.DiGraph(), ndim=3)
-    action1 = AddNodes(
-        tracks, nodes=[0, 1], attributes={"time": [0, 1], "pos": [[0, 1], [1, 2]]}
+    action1 = AddNode(
+        tracks, node=0, attributes={"time": 0, "pos": [0, 1]}
     )
 
     # empty history has no undo or redo
@@ -32,7 +32,7 @@ def test_action_history():
 
     # redo the action
     assert history.redo()
-    assert tracks.graph.number_of_nodes() == 2
+    assert tracks.graph.number_of_nodes() == 1
     assert len(history.undo_stack) == 1
     assert len(history.redo_stack) == 0
     assert history._undo_pointer == 0
@@ -42,7 +42,7 @@ def test_action_history():
 
     # undo and then add new action
     assert history.undo()
-    action2 = AddNodes(tracks, nodes=[10], attributes={"time": [10], "pos": [[0, 1]]})
+    action2 = AddNode(tracks, node=10, attributes={"time": 10, "pos": [0, 1]})
     history.add_new_action(action2)
     assert tracks.graph.number_of_nodes() == 1
     # there are 3 things on the stack: action1, action1's inverse, and action 2
@@ -53,7 +53,7 @@ def test_action_history():
     # undo back to after action 1
     assert history.undo()
     assert history.undo()
-    assert tracks.graph.number_of_nodes() == 2
+    assert tracks.graph.number_of_nodes() == 1
 
     assert len(history.undo_stack) == 3
     assert len(history.redo_stack) == 2
