44ee8e70610bcaf099545f3c1445eb561effb6d9
AnniekStok <anniek.stokkermans@gmail.com>
2025-08-06 17:29:04 +0200

    update tracks tests to ensure 100% coverage

--- tests/data_model/test_tracks.py
+++ tests/data_model/test_tracks.py
@@ -66,9 +66,6 @@ def test_pixels_and_seg_id(graph_3d, segmentation_3d):
     new_seg_id = 10
     tracks.set_pixels(pix, [new_seg_id])
 
-    with pytest.raises(KeyError):
-        tracks.get_positions(["0"])
-
 
 def test_save_load_delete(tmp_path, graph_2d, segmentation_2d):
     tracks_dir = tmp_path / "tracks"
@@ -135,27 +132,122 @@ def test_iou_methods(graph_2d):
 
 def test_get_set_node_attr(graph_2d):
     tracks = Tracks(graph_2d, ndim=3)
+
     tracks._set_node_attr(1, "a", 42)
-    assert tracks.get_node_attr(1, "a") == 42
+    # test deprecated functions
+    with pytest.warns(
+        DeprecationWarning,
+        match="_get_node_attr deprecated in favor of public method get_node_attr",
+    ):
+        assert tracks._get_node_attr(1, "a") == 42
+
     tracks._set_nodes_attr([1, 2], "b", [7, 8])
-    assert tracks.get_nodes_attr([1, 2], "b") == [7, 8]
+    with pytest.warns(
+        DeprecationWarning,
+        match="_get_nodes_attr deprecated in favor of public method get_nodes_attr",
+    ):
+        assert tracks._get_nodes_attr([1, 2], "b") == [7, 8]
+
+    # test new functions
+    assert tracks.get_node_attr(1, "a", required=True) == 42
+    assert tracks.get_nodes_attr([1, 2], "b", required=True) == [7, 8]
+    assert tracks.get_nodes_attr([1, 2], "b", required=False) == [7, 8]
+    with pytest.raises(KeyError):
+        tracks.get_node_attr(1, "not_present", required=True)
+    assert tracks.get_node_attr(1, "not_present", required=False) is None
+    with pytest.raises(KeyError):
+        tracks.get_nodes_attr([1, 2], "not_present", required=True)
+    assert all(
+        x is None for x in tracks.get_nodes_attr([1, 2], "not_present", required=False)
+    )
+
+    # test array attributes
+    tracks._set_node_attr(1, "array_attr", np.array([1, 2, 3]))
+    tracks._set_nodes_attr((1, 2), "array_attr2", np.array(([1, 2, 3], [4, 5, 6])))
 
 
 def test_get_set_edge_attr(graph_2d):
     tracks = Tracks(graph_2d, ndim=3)
     tracks._set_edge_attr((1, 2), "c", 99)
     assert tracks.get_edge_attr((1, 2), "c") == 99
-    tracks._set_edges_attr([(1, 2)], "d", [123])
-    assert tracks.get_edges_attr([(1, 2)], "d") == [123]
+    assert tracks.get_edge_attr((1, 2), "iou", required=True) == 0.0
+    tracks._set_edges_attr([(1, 2), (1, 3)], "d", [123, 5])
+    assert tracks.get_edges_attr([(1, 2), (1, 3)], "d", required=True) == [123, 5]
+    assert tracks.get_edges_attr([(1, 2), (1, 3)], "d", required=False) == [123, 5]
+    with pytest.raises(KeyError):
+        tracks.get_edge_attr((1, 2), "not_present", required=True)
+    assert tracks.get_edge_attr((1, 2), "not_present", required=False) is None
+    with pytest.raises(KeyError):
+        tracks.get_edges_attr([(1, 2), (1, 3)], "not_present", required=True)
+    assert all(
+        x is None
+        for x in tracks.get_edges_attr(((1, 2), (1, 3)), "not_present", required=False)
+    )
+
+
+def test_set_positions_str(graph_2d):
+    tracks = Tracks(graph_2d, ndim=3)
+    tracks.set_positions((1, 2), [(1, 2), (3, 4)])
+    assert np.array_equal(
+        tracks.get_positions((1, 2), incl_time=False), np.array([[1, 2], [3, 4]])
+    )
+    assert np.array_equal(
+        tracks.get_positions((1, 2), incl_time=True), np.array([[0, 1, 2], [1, 3, 4]])
+    )
+
+    # test invalid node id
+    with pytest.raises(KeyError):
+        tracks.get_positions(["0"])
+
+
+def test_set_positions_list(graph_2d_list):
+    tracks = Tracks(graph_2d_list, pos_attr=["y", "x"], ndim=3)
+    tracks.set_positions((1, 2), [(1, 2), (3, 4)])
+    assert np.array_equal(
+        tracks.get_positions((1, 2), incl_time=False), np.array([[1, 2], [3, 4]])
+    )
+    assert np.array_equal(
+        tracks.get_positions((1, 2), incl_time=True), np.array([[0, 1, 2], [1, 3, 4]])
+    )
+
+
+def test_set_node_attributes(graph_2d, caplog):
+    tracks = Tracks(graph_2d, ndim=3)
+    attrs = {"attr_1": [1, 2, 3, 4, 5, 6], "attr_2": ["a", "b", "c", "d", "e", "f"]}
+    tracks._set_node_attributes([1, 2, 3, 4, 5, 6], attrs)
+    assert np.array_equal(tracks.get_nodes_attr([1, 2], "attr_1"), np.array([1, 2]))
+    with caplog.at_level("INFO"):
+        tracks._set_node_attributes([1, 2, 3, 4, 5, 7], attrs)
+    assert any("Node 7 not found in the graph." in message for message in caplog.messages)
+
+
+def test_set_edge_attributes(graph_2d, caplog):
+    tracks = Tracks(graph_2d, ndim=3)
+    attrs = {"attr_1": [1, 2, 3, 4], "attr_2": ["a", "b", "c", "d"]}
+    tracks._set_edge_attributes([(1, 2), (1, 3), (3, 4), (4, 5)], attrs)
+    assert np.array_equal(
+        tracks.get_edges_attr([(1, 2), (1, 3), (3, 4), (4, 5)], "attr_1"),
+        np.array([1, 2, 3, 4]),
+    )
+    with caplog.at_level("INFO"):
+        tracks._set_edge_attributes([(1, 2), (1, 3), (3, 4), (4, 6)], attrs)
+    assert any(
+        "Edge (4, 6) not found in the graph." in message for message in caplog.messages
+    )
 
 
 def test_compute_node_attrs(graph_2d, segmentation_2d):
-    tracks = Tracks(graph_2d, segmentation=segmentation_2d, ndim=3)
+    tracks = Tracks(graph_2d, segmentation=segmentation_2d, ndim=3, scale=(1, 2, 2))
     attrs = tracks._compute_node_attrs([1, 2], [0, 1])
     assert NodeAttr.POS.value in attrs
     assert NodeAttr.AREA.value in attrs
-    assert attrs[NodeAttr.AREA.value][0] == 1245
-    assert attrs[NodeAttr.AREA.value][1] == 305
+    assert attrs[NodeAttr.AREA.value][0] == 1245 * 4
+    assert attrs[NodeAttr.AREA.value][1] == 305 * 4
+
+    # cannot compute node attributes without segmentation
+    tracks = Tracks(graph_2d, segmentation=None, ndim=3)
+    attrs = tracks._compute_node_attrs([1, 2], [0, 1])
+    assert not bool(attrs)
 
 
 def test_compute_edge_attrs(graph_2d, segmentation_2d):
@@ -164,6 +256,11 @@ def test_compute_edge_attrs(graph_2d, segmentation_2d):
     assert EdgeAttr.IOU.value in attrs
     assert attrs[EdgeAttr.IOU.value][0] == 0.0
     assert np.isclose(attrs[EdgeAttr.IOU.value][1], 0.395, rtol=1e-2)
+
+    # cannot compute IOU without segmentation
+    tracks = Tracks(graph_2d, segmentation=None, ndim=3)
+    attrs = tracks._compute_edge_attrs([(1, 2), (1, 3)])
+    assert not bool(attrs)
 
 
 def test_get_pixels_and_set_pixels(graph_2d, segmentation_2d):
@@ -198,44 +295,10 @@ def test_compute_ndim_errors():
     g.add_node(1, time=0, pos=[0, 0, 0])
     # seg ndim = 3, scale ndim = 2, provided ndim = 4 -> mismatch
     seg = np.zeros((2, 2, 2))
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="Dimensions from segmentation"):
         Tracks(g, segmentation=seg, scale=[1, 2], ndim=4)
 
-
-def test_get_node_attr(graph_2d):
-    tracks = Tracks(graph_2d, ndim=3)
-    assert tracks.get_node_attr(1, "time", required=True) == 0
-    with pytest.raises(KeyError):
-        tracks.get_node_attr(1, "not_present", required=True)
-    assert tracks.get_node_attr(1, "not_present", required=False) is None
-
-
-def test_get_nodes_attr(graph_2d):
-    tracks = Tracks(graph_2d, ndim=3)
-    assert tracks.get_nodes_attr([1, 2], "time", required=True) == [0, 1]
-    assert tracks.get_nodes_attr([1, 2], "time", required=False) == [0, 1]
-    with pytest.raises(KeyError):
-        tracks.get_nodes_attr([1, 2], "not_present", required=True)
-    assert all(
-        x is None for x in tracks.get_nodes_attr([1, 2], "not_present", required=False)
-    )
-
-
-def test_get_edge_attr(graph_2d):
-    tracks = Tracks(graph_2d, ndim=3)
-    assert tracks.get_edge_attr((1, 2), "iou", required=True) == 0.0
-    with pytest.raises(KeyError):
-        tracks.get_edge_attr((1, 2), "not_present", required=True)
-    assert tracks.get_edge_attr((1, 2), "not_present", required=False) is None
-
-
-def test_get_edges_attr(graph_2d):
-    tracks = Tracks(graph_2d, ndim=3)
-    assert tracks.get_edges_attr([(1, 2)], "iou", required=True) == [0.0]
-    assert tracks.get_edges_attr([(1, 2)], "iou", required=False) == [0.0]
-    with pytest.raises(KeyError):
-        tracks.get_edges_attr([(1, 2), (1, 3)], "not_present", required=True)
-    assert all(
-        x is None
-        for x in tracks.get_edges_attr(((1, 2), (1, 3)), "not_present", required=False)
-    )
+    with pytest.raises(
+        ValueError, match="Cannot compute dimensions from segmentation or scale"
+    ):
+        Tracks(g)
