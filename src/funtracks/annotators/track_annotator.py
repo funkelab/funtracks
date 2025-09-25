12953b7bbb46a5c2442ad08c60c0767fef67cc89
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-09-24 20:43:31 -0400

    refactor: :art: Move repeated code into functions and add docstrings for track annotator

--- src/funtracks/annotators/track_annotator.py
+++ src/funtracks/annotators/track_annotator.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+from collections import defaultdict
+from typing import TYPE_CHECKING
+
 import networkx as nx
 
 from funtracks.data_model import SolutionTracks
 
 from ..features.feature import Feature, FeatureType
 from .graph_annotator import GraphAnnotator
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 
 class TrackletID(Feature):
@@ -38,6 +44,20 @@ class TrackAnnotator(GraphAnnotator):
     The possible features include:
     - TrackletID
     - LineageID
+
+
+    Args:
+        tracks (SolutionTracks): The tracks to be annotated. Must be a solution.
+        tracklet_key (str | None, optional): A key that already holds the tracklet ids
+            on the graph. If provided, must be there for every node and already hold
+            valid tracklet ids. Defaults to None.
+        lineage_key (str | None, optional): A key that already holds the lineage ids
+            on the graph. If provided, must be there for every node and already hold
+            valid lineage ids. Defaults to None.
+
+    Raises:
+        ValueError: if the provided Tracks are not SolutionTracks (not a binary lineage
+            tree)
     """
 
     def __init__(
@@ -61,32 +81,39 @@ class TrackAnnotator(GraphAnnotator):
         self.max_lineage_id = 0
 
         if tracklet_key is not None and tracks.graph.number_of_nodes() > 0:
-            self.max_tracklet_id = max(
-                tracks.get_nodes_attr(tracks.nodes(), self.tracklet.key)
-            )
-            for node in tracks.nodes():
-                tra_id: int = tracks.get_node_attr(node, self.tracklet.key)
-                if tra_id not in self.tracklet_id_to_node:
-                    self.tracklet_id_to_node[tra_id] = []
-                self.tracklet_id_to_node[tra_id].append(node)
+            max_id, id_to_nodes = self._get_max_id_and_map(self.tracklet.key)
+            self.max_tracklet_id = max_id
+            self.tracklet_id_to_node = id_to_nodes
 
         if lineage_key is not None and tracks.graph.number_of_nodes() > 0:
-            self.max_lineage_id = max(
-                tracks.get_nodes_attr(tracks.nodes(), self.lineage.key)
-            )
-            for node in tracks.nodes():
-                lin_id: int = tracks.get_node_attr(node, self.lineage.key)
-                if lin_id not in self.lineage_id_to_node:
-                    self.lineage_id_to_node[lin_id] = []
-                self.lineage_id_to_node[lin_id].append(node)
+            max_id, id_to_nodes = self._get_max_id_and_map(self.lineage.key)
+            self.max_lineage_id = max_id
+            self.lineage_id_to_node = id_to_nodes
+
+    def _get_max_id_and_map(self, key: str) -> tuple[int, dict[int, list[int]]]:
+        """Get the maximum ID value and a mapping from ids to nodes with that id.
+
+        Args:
+            key (str): The key holding the id attribute. Can be for tracklets or lineages.
+
+        Returns:
+            tuple[int, dict[int, list[int]]]: The maximum id value, and a mapping from
+                ids to a list of nodes with that id.
+        """
+        id_to_nodes = defaultdict(list)
+        for node in self.tracks.nodes():
+            _id: int = self.tracks.get_node_attr(node, key)
+            id_to_nodes[_id].append(node)
+        max_id = max(id_to_nodes.keys()) if len(id_to_nodes) > 0 else 0
+        return max_id, dict(id_to_nodes)
 
     def compute(self, add_to_set=False) -> None:
         """Compute the currently included features and add them to the tracks.
 
         Args:
             add_to_set (bool, optional): Whether to add the Features to the Tracks
-            FeatureSet. Defaults to False. Should usually be set to True on the initial
-            computation, but False on subsequent re-computations.
+                FeatureSet. Defaults to False. Should usually be set to True on the
+                initial computation, but False on subsequent re-computations.
         """
         if add_to_set:
             self.add_features_to_set()
@@ -96,44 +123,62 @@ class TrackAnnotator(GraphAnnotator):
         if self.lineage in self.features:
             self._assign_lineage_ids()
 
-    def _assign_lineage_ids(self) -> None:
-        lineage_id = 1
+    def _assign_ids(
+        self, components: Iterable[Iterable[int]], key: str
+    ) -> tuple[int, dict[int, list[int]]]:
+        """Assign a unique id to each component in the list and return the max used.
 
-        for lineage in nx.weakly_connected_components(self.tracks.graph):
-            nodes = list(lineage)
-            lineage_ids = [
-                lineage_id,
+        IDs start at 1 and are sequential from there.
+
+        Args:
+            components (Iterable[Iterable[int]]): A list of sets of nodes to be labeled
+                uniquely. Can be tracklets or lineages.
+            key (str): the key to save the unique id at on the graph nodes
+
+        Returns:
+            tuple[int, dict[int, list[int]]]: the maximum id used, and a mapping from ids
+                to lists of nodes with that id
+        """
+        id_to_node = {}
+        _id = 1
+        for component in components:
+            nodes = list(component)
+            ids = [
+                _id,
             ] * len(nodes)
-            self.tracks._set_nodes_attr(nodes, self.lineage.key, lineage_ids)
+            self.tracks._set_nodes_attr(nodes, key, ids)
 
-            self.lineage_id_to_node[lineage_id] = nodes
-            lineage_id += 1
-        self.max_lineage_id = lineage_id - 1
+            id_to_node[_id] = nodes
+            _id += 1
+        return _id - 1, id_to_node
+
+    def _assign_lineage_ids(self) -> None:
+        """Add a lineage id attribute to each node of the solution tracks.
+
+        Each connected component will get a unique id, and the relevant class
+        attributes will be updated.
+        """
+        lineages = nx.weakly_connected_components(self.tracks.graph)
+        max_id, ids_to_nodes = self._assign_ids(lineages, self.lineage.key)
+        self.max_lineage_id = max_id
+        self.lineage_id_to_node = ids_to_nodes
 
     def _assign_tracklet_ids(self) -> None:
-        """Add a tracklet_id attribute to a graph by removing division edges,
-        assigning one id to each connected component. Also sets the max_tracklet_id and
-        initializes a dictionary from tracklet_id to nodes
+        """Add a tracklet id attribute to each node of the solution tracks.
+
+        After removing division edges, each connected component will get a unique ID,
+        and the relevant class attributes will be updated.
         """
         graph_copy = self.tracks.graph.copy()
-        tracklet_id = 1
-
         parents = [node for node, degree in self.tracks.graph.out_degree() if degree >= 2]
-        intertrack_edges = []
 
         # Remove all intertrack edges from a copy of the original graph
         for parent in parents:
-            daughters = [child for p, child in self.tracks.graph.out_edges(parent)]
+            daughters = self.tracks.successors(parent)
             for daughter in daughters:
                 graph_copy.remove_edge(parent, daughter)
-                intertrack_edges.append((parent, daughter))
 
-        for tracklet in nx.weakly_connected_components(graph_copy):
-            nodes = list(tracklet)
-            tracklet_ids = [
-                tracklet_id,
-            ] * len(nodes)
-            self.tracks._set_nodes_attr(nodes, self.tracklet.key, tracklet_ids)
-            self.tracklet_id_to_node[tracklet_id] = list(tracklet)
-            tracklet_id += 1
-        self.max_tracklet_id = tracklet_id - 1
+        tracklets = nx.weakly_connected_components(graph_copy)
+        max_id, ids_to_nodes = self._assign_ids(tracklets, self.tracklet.key)
+        self.max_tracklet_id = max_id
+        self.tracklet_id_to_node = ids_to_nodes
