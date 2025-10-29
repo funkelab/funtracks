c5b5cb08fc782dcbdb7759478031a862784bd959
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-10-29 08:04:17 -0700

    Add back in NodeAttr and EdgeAttr enums with deprecation warnings

--- src/funtracks/data_model/__init__.py
+++ src/funtracks/data_model/__init__.py
@@ -1,4 +1,4 @@
 from .tracks import Tracks  # noqa
 from .solution_tracks import SolutionTracks  # noqa
 from .tracks_controller import TracksController  # noqa
-from .graph_attributes import NodeType  # noqa
+from .graph_attributes import NodeType, NodeAttr, EdgeAttr  # noqa
--- src/funtracks/data_model/graph_attributes.py
+++ src/funtracks/data_model/graph_attributes.py
@@ -16,14 +16,60 @@ class DeprecatedEnumMeta(EnumMeta):
             and name not in ("name", "value")
             and isinstance(value, cls)
         ):
-            warnings.warn(
-                f"NodeType.{name} is deprecated and will be removed in funtracks v2.0. "
-                "This is a visualization concern and should be moved to motile_tracker.",
-                DeprecationWarning,
-                stacklevel=2,
-            )
+            enum_name = cls.__name__
+
+            # Customize message based on enum type
+            if enum_name == "NodeType":
+                message = (
+                    f"NodeType.{name} is deprecated and will be removed in "
+                    "funtracks v2.0. This is a visualization concern and "
+                    "should be moved to motile_tracker."
+                )
+            elif enum_name == "NodeAttr":
+                message = (
+                    f"NodeAttr.{name} is deprecated and will be removed in "
+                    "funtracks v2.0. Use string keys from tracks.features "
+                    "instead (e.g., tracks.features.position_key)."
+                )
+            elif enum_name == "EdgeAttr":
+                message = (
+                    f"EdgeAttr.{name} is deprecated and will be removed in "
+                    "funtracks v2.0. Use string keys directly (e.g., 'iou')."
+                )
+            else:
+                message = (
+                    f"{enum_name}.{name} is deprecated and will be removed "
+                    "in funtracks v2.0."
+                )
+
+            warnings.warn(message, DeprecationWarning, stacklevel=2)
 
         return value
+
+
+class NodeAttr(Enum, metaclass=DeprecatedEnumMeta):
+    """Node attributes that can be added to candidate graph.
+
+    .. deprecated:: 2.0
+        NodeAttr enum will be removed in funtracks v2.0. Use string keys from
+        tracks.features instead (e.g., tracks.features.position_key, "area", etc.).
+    """
+
+    POS = "pos"
+    TIME = "time"
+    AREA = "area"
+    TRACK_ID = "track_id"
+
+
+class EdgeAttr(Enum, metaclass=DeprecatedEnumMeta):
+    """Edge attributes that can be added to candidate graph.
+
+    .. deprecated:: 2.0
+        EdgeAttr enum will be removed in funtracks v2.0. Use string keys directly
+        (e.g., "iou").
+    """
+
+    IOU = "iou"
 
 
 class NodeType(Enum, metaclass=DeprecatedEnumMeta):
